c2a4f796bc8c55818d8e7f72752a0cf5232148e5
tech475 <71052219+tech475@users.noreply.github.com>
2024-06-05 23:56:12 +0300

    Add files via upload

---
+++ Change Background Color.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Change Background Color
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Change background color of www.younow.com to a random hexadecimal color
+// @author       You
+// @match        https://www.younow.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function getRandomColor() {
+        return '#' + Math.floor(Math.random()*16777215).toString(16);
+    }
+
+    setInterval(function() {
+        document.body.style.backgroundColor = getRandomColor();
+    }, 1000);
+})();
